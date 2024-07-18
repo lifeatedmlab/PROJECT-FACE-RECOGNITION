02fc0d4d55b67ae646f9eac578efe5366d49a203
aisyarahmar <aisyarahmar06@gmail.com>
2024-07-18 21:52:58 +0700

    baru button

--- faceRecognition_files/app.py
+++ faceRecognition_files/app.py
@@ -210,6 +210,18 @@ def submit_absensi():
     
     return jsonify({'message': 'Data saved successfully',  'waktu': waktu_sekarang_wib}), 200
 
+@app.route('/index/<kodeAnggota>', methods=['DELETE'])
+def delete_data(kodeAnggota):
+    try:
+        mycursor.execute("DELETE FROM img_dataset WHERE kodeAnggota = %s", (kodeAnggota,))
+        mycursor.execute("DELETE FROM usermstr WHERE kodeAnggota = %s", (kodeAnggota,))
+        mydb.commit()
+        
+        return jsonify({'success': True, 'message': f"Data for kodeAnggota {kodeAnggota} deleted successfully."})
+    except Exception as e:
+        mydb.rollback()
+        return jsonify({'success': False, 'message': str(e)})
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     socketio.run(app, host='127.0.0.1', port=5000, debug=True)
