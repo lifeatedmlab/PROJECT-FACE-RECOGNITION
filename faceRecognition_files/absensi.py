6a0cecf37c23d6b782811cf46550758b437d8499
aviesena <aviesenarabbani@gmail.com>
2024-08-14 14:59:54 +0700

    hapus comment dan file yang tidak terpakai

--- faceRecognition_files/absensi.py
+++ faceRecognition_files/absensi.py
@@ -1,6 +1,5 @@
 from database import mydb, mycursor
 
-
 def add_attendance(eventId, kodeAnggota,  waktu):
     sql = "INSERT INTO absensi (eventId, kodeAnggota, waktu) VALUES (%s, %s, %s)"
     val = (eventId, kodeAnggota,  waktu)
